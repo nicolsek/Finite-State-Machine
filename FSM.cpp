f69ab58c89d1a3f4146abf1618fcd0bb724534fb
GeekishGirl <tusek.nicole@gmail.com>
2017-09-11 21:03:43 -0700

    Added the primary files and functions.

---
+++ FSM.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <vector>
+
+//States for the state creation.
+enum StateType {
+	undefined, //If the state is undefined it will default to 0.
+
+	state0,
+	state1,
+	state2,
+	state3,
+
+	final, //Final state to iterate through.
+};
+
+/**
+ * @desc A single state for the stack.
+ */
+class State {
+	public:
+		/* Public Constructor */
+		State(StateType); //Public constructor.
+
+		/* Local Members */
+		StateType getType(); //Returns the state type of the state.
+
+	private:
+		StateType stateType; //The type of state this is.
+};
+
+/**
+ * @desc The public constructor for the state.
+ * @param {StateType} type the type of state for the state.
+ */
+State::State(StateType type) {
+	this->stateType = type; //Set this state type for the state.
+}
+
+/**
+ * @desc Get the type of this state.
+ * @return {StateType} the type of state.
+ */
+StateType State::getType() {
+	return this->stateType;
+}
+
+/**
+ * @desc The stack for the FSM.
+ */
+class Stack {
+	public:
+		/* Public Constructor */
+		Stack();
+		
+		/* Local Members */
+		void addState(State); //Add a state to the top of the stack.
+		void popState(); //Pop the state at the top of the stack.
+
+		/* Debug Members */
+		void printStack(); //Print the stack.
+
+	private:
+		std::vector<State> stack; //The stack.
+};
+
+/**
+ * @desc Public constructor for the stack.
+ */
+Stack::Stack() {
+
+}
+
+/**
+ * @desc Add a state to the top of the stack.
+ * @param {State} state the state to be pushed back.
+ */
+void Stack::addState(State state) {
+	this->stack.push_back(state);
+}
+
+/**
+ * @desc Pop the state from the top of the stack.
+ */
+void Stack::popState() {
+	this->stack.pop_back(); 
+}
+
+/**
+ * @desc Print the contents of the stack.
+ */
+void Stack::printStack() {
+	for (auto &state : this->stack) {
+		std::cout << "State Type --> " << int(state.getType()) << std::endl; //Get the states type, cast it to an int, and print it. 
+	}
+}
+
+/**
+ * @desc The main function.
+ */
+int main() {
+	Stack stack;
+
+	for (int stateType = state0; stateType != StateType::final; ++stateType) { //Get the first state and increase every state until done.
+		State state = State(StateType(stateType)); //Create the new state to add.
+		stack.addState(state); //Add that state to the stack.
+	}
+
+	stack.printStack(); //Print the contents of the stack.
+
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,5 @@
+all:
+	g++ FSM.cpp -o FSM.o -std=c++11 
+
+out:
+	./FSM.o
